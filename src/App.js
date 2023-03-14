e337fddf9d752bccc5a86b9d233b6df2d563febd
Adil <Theadicore@gmail.com>
2023-03-15 01:09:53 +0600

    Added custom files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import { ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed.jsx';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <ChatEngine 
+            height="100vh"
+            projectID="5b3b478b-73a9-4b67-8961-4d36b7f568a7"
+            userName="Askar"
+            userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} />}
+
+        />
+    )
 }
 
-export default App;
+export default App; 
